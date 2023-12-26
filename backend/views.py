99a008d9278a9624698ff85290826bcc958730bc
ligu2006 <138577458+ligu2006@users.noreply.github.com>
2023-12-26 05:25:35 -0500

    Update views.py

--- backend/views.py
+++ backend/views.py
@@ -8,7 +8,7 @@ def index(request):
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel backend!</h1>
+            <h1>Hello from Vercel backend after removing example directory!</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
